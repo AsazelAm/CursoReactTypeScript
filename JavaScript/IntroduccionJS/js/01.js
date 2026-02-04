9ae635e4c9fca78beda0181a074835aeba3237e5
AsazelAm <asafajoruro597@gmail.com>
2026-02-03 21:19:38 -0400

    Tipos de datos ,Variable let const

---
+++ JavaScript/IntroduccionJS/js/01.js
@@ -0,0 +1,21 @@
+//Variables
+//hoy en dia no se recomienda usar var
+/*VARIABLE LET
+- No puede iniciar con numeros ni caracteres especiales al nombrar la variable */
+let name = "hoal";
+console.log(name)
+name = 12;
+console.log(name)
+
+//undefined es una variable que esta inicializada o definida pero no tiene valor
+//ha is not defined : Es una variable que no fue inicializada o definica o no existe
+//console.log(ha)
+let undefina;
+console.log(undefina)
+
+//VARIABLE CONST
+/*Las variables const deben inicilizarse con un valor y no se peuede reasigna su valor mas adelante */
+const number = 15;
+//const n ;
+number = 13;
+console.log(number)
---
+++ JavaScript/IntroduccionJS/js/02.js
@@ -0,0 +1,14 @@
+//TIPOS DE DATOS
+/*Undefined
+Es un tipo de dato que existe pero no esta definido su tipo de datos
+*/
+let undefi;
+console.log(undefi);
+console.log(typeof undefi) //typeof :PARA SABER EL TIPO DE DATO QUE TIENE LA VARIABLE
+
+/*STRING
+Es un tipo de dato para cadenas de texto etc
+*/
+let strin ="hola";
+console.log(strin);
+console.log(typeof strin)
